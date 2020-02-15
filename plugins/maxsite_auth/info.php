caaa790d67e2c7602cf38c10c9f8a9da242b4799
MAX <maxsite.org@gmail.com>
2020-02-15 10:02:01 +0200

    Add maxsite_auth

---
+++ plugins/maxsite_auth/info.php
@@ -0,0 +1,13 @@
+<?php if (!defined('BASEPATH')) exit('No direct script access allowed'); 
+
+$info = array(
+	'name' => t('Авторизация MaxSiteAuth'),
+	'description' => t('Авторизация с помощью своего сайта на чужих сайтах, также работающих на MaxSite CMS. Требуется переделка под PHP 7.1'),
+	'version' => '2.0',
+	'author' => 'Максим',
+	'plugin_url' => '',
+	'author_url' => 'https://maxsite.org/',
+	'group' => 'system'
+);
+
+# end of file
